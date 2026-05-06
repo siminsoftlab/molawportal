635742c67ed744f8207f4a943919a6e1dfd74c26
siminsoftlab <siminsoftlab@gmail.com>
2026-05-06 20:30:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ function resetInputs() {
   document.getElementById('repayResult').innerHTML = '';
 
   // 필요 시 기본값 복원 (예: months, extra, asset)
+  document.getElementById("income").value = "";
+  document.getElementById("living").value = "";
   document.getElementById('extra').value = 0;
   document.getElementById('months').value = 36;
   document.getElementById('asset').value = 0;
