8e352e6535c2fc8988dace1b0f8c75820df03928
siminsoftlab <siminsoftlab@gmail.com>
2026-05-19 17:54:03 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   new daum.roughmap.Lander({
     "timestamp": "1779180009898",
-    "key": "o2q7yjgvpjn",
-    "mapWidth": "640",
-    "mapHeight": "360"
+    "key": "o2q7yjgvpjn"
   }).render();
 });
