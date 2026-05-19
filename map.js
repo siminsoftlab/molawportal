c0ea79d47fb297fb2d119d34a5b84f78b1ccd02d
siminsoftlab <siminsoftlab@gmail.com>
2026-05-19 17:43:45 +0900

    Create map.js

---
+++ map.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new daum.roughmap.Lander({
+    "timestamp": "1779180009898",
+    "key": "o2q7yjgvpjn",
+    "mapWidth": "640",
+    "mapHeight": "360"
+  }).render();
+});
