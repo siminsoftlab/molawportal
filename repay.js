b2345c31f24d8335a88a33a4b0d154c7e8829887
siminsoftlab <siminsoftlab@gmail.com>
2026-05-30 01:25:33 +0900

    Update repay.js

--- repay.js
+++ repay.js
@@ -273,14 +273,14 @@ function toggleAccordionRepay() {
   if (!box || !btn) return;
 
   if (box.classList.contains("open")) {
-    box.classList.remove("open");
-    box.style.maxHeight = null;
-    btn.textContent = "계산 상세 보기 ▼";
-  } else {
-    box.classList.add("open");
-    box.style.maxHeight = box.scrollHeight + "px";
-    btn.textContent = "계산 상세 접기 ▲";
-  }
+      box.classList.remove("open");
+      box.style.maxHeight = "0px";
+      btn.textContent = "계산 상세 보기 ▼";
+    } else {
+      box.classList.add("open");
+      box.style.maxHeight = box.scrollHeight + "px";
+      btn.textContent = "계산 상세 접기 ▲";
+    }
 }
 
 /****************************************************
