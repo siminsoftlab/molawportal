64e243f40f41ca1fdd6ff7532404816d84e09952
siminsoftlab <siminsoftlab@gmail.com>
2026-05-31 22:01:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -315,7 +315,7 @@ document.addEventListener("DOMContentLoaded", () => {
  * 온라인 상담 말풍선 타이핑 효과 (확정 작동 버전)
  ****************************************************/
 document.addEventListener("DOMContentLoaded", function () {
-  const text2 = "회원가입 없이 바로 이용 가능하며, 상담은 무료입니다.";
+  const text2 = "1분이면 상담신청 가능합니다.";
   const typingTarget2 = document.getElementById("consult-typing");
 
   if (!typingTarget2) return;
