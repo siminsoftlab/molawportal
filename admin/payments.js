3be4519bea243c69d36ff7afe05ca046f4d73ed5
siminsoftlab <siminsoftlab@gmail.com>
2026-06-05 16:20:34 +0900

    Update payments.js

--- admin/payments.js
+++ admin/payments.js
@@ -1,20 +1,4 @@
 /* ============================================================
-   Firebase 초기화
-============================================================ */
-const firebaseConfig = {
-  apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
-  authDomain: "molawcounter.firebaseapp.com",
-  projectId: "molawcounter",
-  storageBucket: "molawcounter.firebasestorage.app",
-  messagingSenderId: "989958208701",
-  appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
-  measurementId: "G-D4W34NBWKT"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-/* ============================================================
    이용권 발급 함수
 ============================================================ */
 async function issueToken(userId) {
