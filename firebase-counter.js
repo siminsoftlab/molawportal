b92bb406917a4f9df0a7c6af1686b733e64501b7
siminsoftlab <siminsoftlab@gmail.com>
2026-06-06 12:33:04 +0900

    Update firebase-counter.js

--- firebase-counter.js
+++ firebase-counter.js
@@ -31,24 +31,6 @@ function getTodayString() {
 }
 
 /* ============================================================
-   Firebase 초기화 (v8)
-   ============================================================ */
-const firebaseConfig = {
-  apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
-  authDomain: "molawcounter.firebaseapp.com",
-  projectId: "molawcounter",
-  storageBucket: "molawcounter.firebasestorage.app",
-  messagingSenderId: "989958208701",
-  appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
-  measurementId: "G-D4W34NBWKT"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.firestore();
-
-/* ============================================================
    방문자 업데이트 (고유 방문자 기반)
    ============================================================ */
 async function updateVisitorCount() {
