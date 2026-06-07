9c1f34dccda6040b08c1bcae15e9762fbaa49ef7
siminsoftlab <siminsoftlab@gmail.com>
2026-06-08 03:01:15 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
+/* ============================================================
+   Firebase 초기화 (모든 페이지 공통)
+============================================================ */
+const firebaseConfig = {
+  apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
+  authDomain: "molawcounter.firebaseapp.com",
+  projectId: "molawcounter",
+  storageBucket: "molawcounter.firebasestorage.app",
+  messagingSenderId: "989958208701",
+  appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
+  measurementId: "G-D4W34NBWKT"
+};
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
-    authDomain: "molawcounter.firebaseapp.com",
-    projectId: "molawcounter",
-    storageBucket: "molawcounter.firebasestorage.app",
-    messagingSenderId: "989958208701",
-    appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
-    measurementId: "G-D4W34NBWKT"
-  };
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-  window.auth = firebase.auth();
-  window.db = firebase.firestore();
-
-  console.log("firebase-init.js initialized");
-});
+console.log("firebase-init.js initialized");
