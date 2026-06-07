f0f9b0953f03ea06904f91873741fc5a92ae6cd7
siminsoftlab <siminsoftlab@gmail.com>
2026-06-07 21:11:01 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 /* ============================================================
-   Firebase 초기화
-   ============================================================ */
-const firebaseConfig = {
-  apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
-  authDomain: "molawcounter.firebaseapp.com",
-  projectId: "molawcounter",
-  storageBucket: "molawcounter.firebasestorage.app",
-  messagingSenderId: "989958208701",
-  appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
-  measurementId: "G-D4W34NBWKT"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-/* ============================================================
    관리자 접근 로그 기록
    ============================================================ */
 async function logAdminAccess() {
