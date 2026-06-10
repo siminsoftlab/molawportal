60b71e112ab41fd138728bca794cf466ed8a4181
siminsoftlab <siminsoftlab@gmail.com>
2026-06-11 01:40:26 +0900

    Update login.js

--- auth/login.js
+++ auth/login.js
@@ -1,19 +1,4 @@
-/* ============================================================
-   Firebase 초기화
-============================================================ */
-const firebaseConfig = {
-  apiKey: "AIzaSyACfN4_r2hUAn1NQPWRZzpegjyIESYGK3I",
-  authDomain: "molawcounter.firebaseapp.com",
-  projectId: "molawcounter",
-  storageBucket: "molawcounter.firebasestorage.app",
-  messagingSenderId: "989958208701",
-  appId: "1:989958208701:web:16bd53eed95276f5d4cbd4",
-  measurementId: "G-D4W34NBWKT"
-};
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
 
 /* ============================================================
    로그인 처리
