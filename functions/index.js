b5b37e88cdbfc0f4bddf9a2d74260c9e273423f1
siminsoftlab <siminsoftlab@gmail.com>
2026-07-14 02:55:39 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,6 @@ const cors = require("cors");
 const { GoogleAuth } = require("google-auth-library");
 // Cloud Functions for Document AI
 const fetch = require("node-fetch");
-const cors = require("cors");
 const corsHandler = cors({ origin: true });
 
 admin.initializeApp();
