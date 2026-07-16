ad5ee50d8cf7bce8a1b68615a4cc985a2ef96703
siminsoftlab <siminsoftlab@gmail.com>
2026-07-17 01:18:51 +0900

    Update ocr.js

--- public/ocr/ocr.js
+++ public/ocr/ocr.js
@@ -234,10 +234,7 @@ parseBtn.addEventListener("click", async () => {
 function fileToBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = () => {
-      const base64 = reader.result.split(",")[1];  // 앞부분 제거
-      resolve(base64);
-    };
+    reader.onload = () => resolve(reader.result.split(",")[1]);
     reader.onerror = reject;
     reader.readAsDataURL(file);
   });
