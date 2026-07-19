bb851f9aa71ddf317b86373c0871606b6bc0085f
siminsoftlab <siminsoftlab@gmail.com>
2026-07-20 02:32:24 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,36 @@
 const { onRequest } = require("firebase-functions/v2/https");
+const cors = require("cors");
 const { DocumentProcessorServiceClient } = require("@google-cloud/documentai").v1beta3;
 
 const v1 = require("./v1");
 
 const docAI2 = onRequest(
-  {
-    cors: ["https://molawcalculator.com"],
-  },
+  { timeoutSeconds: 300, memory: "1GiB" },
   async (req, res) => {
-    try {
-      const { base64 } = req.body;
+    cors({ origin: "https://molawcalculator.com" })(req, res, async () => {
+      try {
+        const { base64 } = req.body;
 
-      const client = new DocumentProcessorServiceClient();
+        const client = new DocumentProcessorServiceClient();
 
-      const name =
-        "projects/989958208701/locations/us/processors/f9e461994ba2266a";
+        const name =
+          "projects/989958208701/locations/us/processors/f9e461994ba2266a";
 
-      const request = {
-        name,
-        rawDocument: {
-          content: base64,
-          mimeType: "application/pdf",
-        },
-      };
+        const request = {
+          name,
+          rawDocument: {
+            content: base64,
+            mimeType: "application/pdf",
+          },
+        };
 
-      const [result] = await client.processDocument(request);
+        const [result] = await client.processDocument(request);
 
-      res.status(200).json(result);
-
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
+        res.status(200).json(result);
+      } catch (e) {
+        res.status(500).json({ error: e.message });
+      }
+    });
   }
 );
 
